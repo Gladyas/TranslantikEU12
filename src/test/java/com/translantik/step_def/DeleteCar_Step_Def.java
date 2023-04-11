22eee92427cacb6e1c01d3334a4b1fdee624572f
Tahir Altindas <taltindasforum@gmail.com>
2023-04-11 15:23:42 +0300

    conf

--- src/test/java/com/translantik/step_def/DeleteCar_Step_Def.java
+++ src/test/java/com/translantik/step_def/DeleteCar_Step_Def.java
@@ -14,70 +14,12 @@ public class DeleteCar_Step_Def {
     FleetVehiclePage fleetVehiclePage=new FleetVehiclePage();
     Actions act=new Actions(Driver.getDriver());
 
-    @When("store manager hovering mouse over the three dots")
-    public void storeManagerHoveringMouseOverTheThreeDots()  {
-
-    }
 
 
-    @Then("store manager can see the delete button")
-    public void storeManagerCanSeeTheDeleteButton() {
-    }
 
-    @And("user manager click the delete button")
-    public void userManagerClickTheDeleteButton() {
-    }
 
-    @Then("user should see delete confirmation pop up")
-    public void userShouldSeeDeleteConfirmationPopUp() {
-    }
 
-    @When("driver hovering mouse over the three dots")
-    public void driverHoveringMouseOverTheThreeDots() {
-    }
 
-    @And("user click the delete button")
-    public void userClickTheDeleteButton() {
-    }
-
-    @And("user click the yes delete button")
-    public void userClickTheYesDeleteButton() {
-    }
-
-    @Then("You do not have permission to perform this action message should be displayed")
-    public void youDoNotHavePermissionToPerformThisActionMessageShouldBeDisplayed() {
-    }
-
-    @When("sales manager hovering mouse over the three dots")
-    public void salesManagerHoveringMouseOverTheThreeDots() {
-    }
-    @Then("Item deleted message should be displayed.")
-    public void ıtemDeletedMessageShouldBeDisplayed() {
-    }
-
-    @When("sales manager click any vehicle row")
-    public void salesManagerClickAnyVehicleRow() {
-    }
-
-    @And("click information page delete button")
-    public void clickInformationPageDeleteButton() {
-    }
-
-    @When("When sales manager click any vehicle row")
-    public void whenSalesManagerClickAnyVehicleRow() {
-    }
-
-    @And("user get the Chassis Number")
-    public void userGetTheChassisNumber() {
-    }
-
-    @Then("user should check corresponding car also be removed from the Fleet-Vehicle page")
-    public void userShouldCheckCorrespondingCarAlsoBeRemovedFromTheFleetVehiclePage() {
-    }
-
-    @When("When store manager click any vehicle row")
-    public void whenStoreManagerClickAnyVehicleRow() {
-    }
 
 
 }
