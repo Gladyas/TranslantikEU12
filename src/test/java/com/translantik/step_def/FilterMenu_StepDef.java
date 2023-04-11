c61bf49083427ad53d370a84d13d8c9b1e0eca74
havlenurdemir <hvlnrdmr@gmail.com>
2023-04-11 15:59:04 +0300

    filter menu

--- src/test/java/com/translantik/step_def/FilterMenu_StepDef.java
+++ src/test/java/com/translantik/step_def/FilterMenu_StepDef.java
@@ -1,6 +1,7 @@
 package com.translantik.step_def;
 
 import com.translantik.pages.DashBoardPage;
+import com.translantik.pages.FleetVehiclePage;
 import com.translantik.pages.GridPage;
 import com.translantik.pages.TranslantikLoginPage;
 import com.translantik.utilities.BrowserUtils;
@@ -13,6 +14,7 @@ import org.junit.Assert;
 public class FilterMenu_StepDef {
 
     TranslantikLoginPage translantikLoginPage=new TranslantikLoginPage();
+    FleetVehiclePage fleetVehiclePage=new FleetVehiclePage();
     GridPage gridPage=new GridPage();
     @Given("The user logged in as {string}")
     public void theUserLoggedInAs(String userType) {
@@ -29,13 +31,31 @@ public class FilterMenu_StepDef {
     @And("The user clicks on filterIcon")
     public void theUserClicksOnFilterIcon() {
         gridPage.waitUntilLoaderScreenDisappear();//Added By @CharlieAlfa
+        BrowserUtils.sleep(5);
         BrowserUtils.waitForClickablility(gridPage.filterIcon, 10);
         gridPage.filterIcon.click();
     }
 
     @Then("The user should see Manage Filter button became visible")
     public void theUserShouldSeeManageFilterButtonBecameVisible() {
+        BrowserUtils.sleep(5);
         Assert.assertTrue("Verify Manage Filter button became visible", gridPage.manageFilterButton.isDisplayed());
     }
 
+    @When("user clicks on manage filter button")
+    public void userClicksOnManageFilterButton() {
+        BrowserUtils.sleep(5);
+        BrowserUtils.waitForClickablility(gridPage.filterIcon, 10);
+        gridPage.filterIcon.click();
+        BrowserUtils.sleep(5);
+        gridPage.manageFilterButton.click();
+    }
+
+    @Then("user can click on filters name")
+    public void userCanClickOnFiltersName() {
+        BrowserUtils.sleep(5);
+        gridPage.manageFilterButton.click();
+        BrowserUtils.sleep(5);
+        fleetVehiclePage.tags.click();
+    }
 }
